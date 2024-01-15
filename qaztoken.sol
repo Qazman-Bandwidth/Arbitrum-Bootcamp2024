21830ac2f54d84fbbfe9228b3afe25a5f08dc43c
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2024-01-15 14:11:52 +0100

    Task 5

---
+++ qaztoken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract QAZToken is ERC20 {
+    constructor()
+        ERC20("QAZ Token", "QAZ")
+    {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
